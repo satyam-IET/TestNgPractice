423545d91eb2aa9e75c69622b868f11ae79510c6
satyam <satyamsdet@gmail.com>
2021-06-20 05:43:22 +0000

    waytocreatereports

---
+++ src/Test/ExcelReport.java
@@ -0,0 +1,20 @@
+package Test;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import com.common.excelreport.ExcelReportGenerator;
+
+public class ExcelReport {
+
+	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+		// TODO Auto-generated method stub
+		
+		ExcelReportGenerator.generateExcelReport("ExcelReport.xlsx", "/home/satyamshukla161/eclipse-workspace/TestNg_Example/");
+
+	}
+
+}
--- src/Test/Logintest.java
+++ src/Test/Logintest.java
@@ -7,13 +7,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class Logintest {
 	
 	WebDriver driver;
+	ExtentReports reports;
+	ExtentTest  test;
+	SoftAssert soft = new SoftAssert();
 	@BeforeMethod
 	public void Setup() {
 		
@@ -28,11 +37,15 @@ public class Logintest {
 		//To wait for 5 sec in every page
 		
 		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		
+		reports = new  ExtentReports("ExtentReports.html");
 	}
 	
 	@Test(priority=0 , description = "Verify User is able to click on login button")
 	@Parameters({"username","password"})
 	public void test1(String usrnm , String Pass) {
+		
+		test = reports.startTest("Login Test Case");
 		
 		
 		// To click on any tab
@@ -41,6 +54,9 @@ public class Logintest {
 		
 		LoginLink.click();
 		
+		test.log(LogStatus.PASS, "Successfully clicked on the login button");
+
+		
 
 		//to enter email / to enter any name in webpage
 		
@@ -48,33 +64,58 @@ public class Logintest {
 		
 		UserName.sendKeys(usrnm);
 		
+		test.log(LogStatus.PASS, "Successfully Entered Username");
+
+		
 		// Enter Password
 		
 		WebElement UserPassword = driver.findElement(By.name("user_pwd"));
 		
 		UserPassword.sendKeys(Pass);
 		
+		test.log(LogStatus.PASS, "Successfully Entered Username");
+
+		
 		//Click on Remember me check box
 		
 		WebElement RememberMe =driver.findElement(By.className("rememberMe"));
 		RememberMe.click();
 		
+		test.log(LogStatus.PASS, "Successfully clicked on remember me checkbox");
+
+		
 		WebElement LoginName = driver.findElement(By.name("btn_login"));
 		LoginName.click();
+		
+		test.log(LogStatus.PASS, "Successfully clicked on login button");
+
 		
 		WebElement Error =driver.findElement(By.id("msg_box"));
 		String Actmsg = Error.getText();
 		
-		String expectmsg = "The email or password you have entered  invalid.";
+		String expectmsg = "The email or password you have entered is invali";
 		
 		Assert.assertTrue(Error.isDisplayed());
-		Assert.assertEquals(Actmsg , expectmsg);
 
+		/*try {
+			Assert.assertEquals(Actmsg, expectmsg);
+			test.log(LogStatus.PASS, "Expected and Actual value matches");
+			
+		}catch(Throwable e) {
+			test.log(LogStatus.FAIL, "Expected and Actual value does not match");
+	    } */
 		
-		driver.close();
 		
+
+	}
+	@AfterMethod
+	public void teardown() {
+		reports.endTest(test);
+		reports.flush();
 		
-		}
+		driver.quit();
+		
+	}
 	
 	
 
