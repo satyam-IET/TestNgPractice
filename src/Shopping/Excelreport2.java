bbffe9aaab3338a533a6dcd98e815f88aa627a71
satyam <satyamsdet@gmail.com>
2021-06-20 06:38:42 +0000

    addednewclassNewApplication

---
+++ src/Shopping/Excelreport2.java
@@ -0,0 +1,20 @@
+package Shopping;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import com.common.excelreport.ExcelReportGenerator;
+
+public class Excelreport2 {
+
+	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+		// TODO Auto-generated method stub
+
+		ExcelReportGenerator.generateExcelReport("ExcelReport.xlsx", "/home/satyamshukla161/eclipse-workspace/TestNg_Example/");
+
+	}
+
+}
---
+++ src/Shopping/NewApplication.java
@@ -0,0 +1,95 @@
+package Shopping;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+
+public class NewApplication {
+	
+	WebDriver driver;
+
+	@BeforeMethod
+	public void Setup() {
+		
+        driver = new ChromeDriver();
+		
+		driver.get("https://www.saucedemo.com/");
+		
+		//To maximize the browser 
+		
+		driver.manage().window().maximize();
+		
+		//To wait for 5 sec in every page
+		
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		
+		//reports = new  ExtentReports("ExtentReports.html");
+	}
+	@Test(priority =0 , description ="verify user is able to login")
+	public void Logintest() {
+		
+		WebElement username = driver.findElement(By.xpath("//*[@name='user-name']"));
+	    username.sendKeys("standard_user");
+	    
+	    WebElement password = driver.findElement(By.xpath("//*[@name='password']"));
+	    password.sendKeys("secret_sauce");
+	    
+	    WebElement login = driver.findElement(By.xpath("//*[@id='login-button']"));
+	    login.click();
+	    
+		}
+	@Test(priority =1 , description = "Verify user is able to select item to cart" ,dependsOnMethods = "Logintest")
+	public void selectcart() {
+		
+		WebElement username = driver.findElement(By.xpath("//*[@name='user-name']"));
+	    username.sendKeys("standard_user");
+	    
+	    WebElement password = driver.findElement(By.xpath("//*[@name='password']"));
+	    password.sendKeys("secret_sauce");
+	    
+	    WebElement login = driver.findElement(By.xpath("//*[@id='login-button']"));
+	    login.click();
+		
+		WebElement select = driver.findElement(By.xpath("//*[@name='add-to-cart-sauce-labs-backpack']"));
+		select.click();
+		
+		
+		
+		
+	}
+	@Test(priority = 3 , description ="verify user is able to clcik on cart", dependsOnMethods = "selectcart")
+	public void Opencart() {
+		
+		WebElement username = driver.findElement(By.xpath("//*[@name='user-name']"));
+	    username.sendKeys("standard_user");
+	    
+	    WebElement password = driver.findElement(By.xpath("//*[@name='password']"));
+	    password.sendKeys("secret_sauce");
+	    
+	    WebElement login = driver.findElement(By.xpath("//*[@id='login-button']"));
+	    login.click();
+		
+		WebElement select = driver.findElement(By.xpath("//*[@name='add-to-cart-sauce-labs-backpack']"));
+		select.click();
+		
+		WebElement cart = driver.findElement(By.className("shopping_cart_link"));
+		cart.click();
+		
+	}
+	
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+	
+	
+
+}
